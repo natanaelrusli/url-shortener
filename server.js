1673435efd287dbb4cd4e2c5fb4d67525316da7a
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2020-10-12 15:38:27 +0700

    commit to github

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const ShortUrl = require('./models/shortUrl')
+const port = 3000
+const mongoURI = 'mongodb+srv://admin:admin@cluster0.m11dw.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+mongoose.connection.once("open", () => {
+    console.log("Mongodb CONNECTED");
+});
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls : shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullurl })
+    res.redirect('/')
+})
+
+app.get('/:id', async (req, res) => {
+    const url = await ShortUrl.findById(req.params.id);
+    res.redirect(url.full)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
