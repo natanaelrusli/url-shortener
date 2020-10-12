9dce2273956cc244ae24fa25e53a4b7835e6e328
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2020-10-13 02:38:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.get('/:id', async (req, res) => {
     res.redirect(url.full)
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
